e95a4d363bc483d839f59497b85684c92d2f7da5
Maxence Lavenu <maxence.lavenu@outlook.fr>
2023-12-10 23:02:09 +0100

    Refactor search route to implement caching and handle errors

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,42 +1,45 @@
 import { NextResponse } from 'next/server';
 
+let cache = null;
+
 export const dynamic = 'force-dynamic';
 
-
-
 export async function GET(request) {
-  // return new NextResponse.json("Hello World");
-
   try {
-    // Get the search query
     const query = request.nextUrl.searchParams.get('query');
     const languages = request.nextUrl.searchParams.get('languages');
-    const page = request.nextUrl.searchParams.get('page');
+    let page = request.nextUrl.searchParams.get('page');
 
-    // get the items located at /data/items.json
-    const items = await import('.//../../../data/items.json')
+    if (!cache) {
+      cache = await import('.//../../../data/items.json');
+    }
+    const items = cache;
 
-    // split the languages string into an array
-    const languagesArray = languages.split(',')
+
+    let languagesArray = [];
+    if (languages) {
+      languagesArray = languages.split(',')
+    }
 
     const filteredItems = Object.fromEntries(Object.entries(items).filter(([id, item]) => {
-      // Check if the item name in either selected languages contains the search query
       return languagesArray.some((language) => {
         if (item[language]) {
-          return item[language].toLowerCase().includes(query.toLowerCase())
+          return item[language].toLowerCase().includes(query ? query.toLowerCase() : '')
         } else {
           return false
         }
       })
     }))
 
-    // Split it into pages of 20 items
+    if (!page || isNaN(page)) {
+      page = 1;
+    }
+
     const pageItems = Object.fromEntries(Object.entries(filteredItems).slice((page - 1) * 20, page * 20))
 
-    // Return the filtered items as an array
     return NextResponse.json(pageItems)
   } catch (error) {
-    console.log('Error loading search results in!')
-    return new NextResponse(error)
+    console.log('Error loading search results in!', error)
+    return new NextResponse(500, { error: 'Internal Server Error' })
   }
 }
